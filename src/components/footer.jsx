f2bb19cfea7a48bf66d668bd5f5c43b9f4bad631
Christian Limbrick <sixo@Christians-Air.attlocal.net>
2023-11-17 18:03:52 -0600

    footer update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,10 +3,10 @@ import "../index.css"
 export default function Footer() {
   return (
     <div className="flex flex-col justify-center items-center bg-gray-800 mt-7">
-      <h1 className="text-xl text-center font-bold text-white pt-6" style={{ minHeight: '10vh' }}>
-        Created by Christian A Limbrick
+      <h1 className="text-m text-center text-white pt-6" style={{ minHeight: '10vh' }}>
+        Created by Christian Limbrick
       </h1>
-      <p className="text-center text-white pb-4">
+      <p className="text-center text-sm text-white pb-4">
   <a href="mailto:avelimb@gmail.com" className="text-white">
     avelimb@gmail.com
   </a>
